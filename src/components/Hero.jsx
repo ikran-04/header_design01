e4d8d9a3e720db3921d4ae20367dcf54c41860f2
ikran-04 <hikraan581@gmail.com>
2023-10-22 10:26:22 +0300

    final commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,11 @@ const Hero = () => {
     <div className='w-full h-4/5  flex flex-col md:flex-row justify-between items-center'>
             <div className='md:w-1/3 p-5 z-10 space-y-5'>
                 <p className='text-green-500 font-bold'>Plants Shop</p>
-                <h1 className='text-5xl md:text-7xl leading-snug'>Home Is <span className='font-bold'>Where My <span className='rounded-md px-2 bg-green-500 text-white'>Plants</span> Are</span></h1>
-                <p className='text-sm text-gray-500'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, sequi porro pariatur voluptate, obcaecati velit, quod quibusdam ullam dolorem facere iure laborum? Beatae laudantium iusto incidunt quibusdam ea!</p>
+                <h1 className='text-7xl leading-[6rem] '>Home Is <span className='font-bold'>Where My <span className='rounded-md px-2 bg-green-500 text-white'>Plants</span> Are</span></h1>
+                <p className='text-sm text-gray-500 '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, sequi porro pariatur voluptate, obcaecati velit, quod quibusdam ullam dolorem facere iure laborum? Beatae laudantium iusto incidunt quibusdam ea!</p>
                 <div className='flex items-center space-x-5'>
                     <button className='px-5 py-2 bg-green-500 text-white font-bold rounded-md'>Shop Plants</button>
-                    <button>Watch Our Videos</button>
+                    <button className='flex items-center space-x-2'><img src="/play.png" alt="" className='w-8 h-8'/> <span>Watch Our Videos</span></button>
                 </div>
             </div>
             <div className="hidden md:flex justify-center z-0">
@@ -19,7 +19,7 @@ const Hero = () => {
             <div className='md:w-1/4 h-full p-5 flex flex-col justify-around z-10 space-y-5 '>
                 <img src="shape.png" alt="" />
                 <div className='space-y-4'>
-                    <h1 className='text-3xl font-bold'>Silver Photos</h1>
+                    <h1 className='text-3xl font-bold'>MONTERA</h1>
                     <p className='text--xl font-bold'>5 color available</p>
                     <p className='text-sm text-gray-500'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem soluta possimus fugit alias. Iure, doloribus!</p>
                     <button className='font-bold flex items-center'><span>Shop Now</span> 
